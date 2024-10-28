5edce55e86db90a4e596c23b41ba8bf228519255
jmechristian <jmechristian@gmail.com>
2024-10-28 10:25:21 -0400

    ssss

--- lib/components/utility/ImageSelector/index.tsx
+++ lib/components/utility/ImageSelector/index.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import Pagination from '../Pagination';
 
 export const ImageSelector = ({
   images,
@@ -10,6 +11,7 @@ export const ImageSelector = ({
   returnSelected: (selected: string[]) => void;
 }) => {
   const [selectedImages, setSelectedImages] = useState<string[]>(selected);
+  const [isCurrentPage, setIsCurrentPage] = useState<number>(1);
   const [searchTerm, setSearchTerm] = useState<string>('');
 
   useEffect(() => {
@@ -24,13 +26,26 @@ export const ImageSelector = ({
     );
   };
 
+  const GFG = (array: string[], currentPage: number, pageSize: number) => {
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    return array.slice(startIndex, endIndex);
+  };
+
   const filteredImages = images.filter((image) =>
     image.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const paginatedItems = useMemo(() => {
+    if (filteredImages) {
+      const currentPageData = GFG(filteredImages, isCurrentPage, 16);
+      return currentPageData;
+    }
+  }, [filteredImages, isCurrentPage]);
+
   return (
-    <div>
-      <div className='flex mb-4'>
+    <div className='w-full min-w-[500px] max-w-[800px] mx-auto flex flex-col gap-8'>
+      <div className='flex'>
         <input
           type='text'
           placeholder='Search...'
@@ -41,14 +56,26 @@ export const ImageSelector = ({
         <button className='p-2 bg-blue-500 text-white rounded'>Save</button>
       </div>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-        {filteredImages.map((image) => (
-          <div
-            key={image}
-            onClick={() => toggleSelectImage(image)}
-            className={`w-24 h-24 bg-cover cursor-pointer ${selectedImages.includes(image) ? 'border-4 border-green-500' : ''}`}
-            style={{ backgroundImage: `url(${image})` }}
-          />
-        ))}
+        {paginatedItems ? (
+          paginatedItems.map((image) => (
+            <div
+              key={image}
+              onClick={() => toggleSelectImage(image)}
+              className={`w-full aspect-square bg-cover cursor-pointer ${selectedImages.includes(image) ? 'border-4 border-green-500' : ''}`}
+              style={{ backgroundImage: `url(${image})` }}
+            />
+          ))
+        ) : (
+          <div>No images found</div>
+        )}
+      </div>
+      <div>
+        <Pagination
+          totalItems={filteredImages.length}
+          itemsPerPage={16}
+          currentPage={isCurrentPage}
+          onPageChange={setIsCurrentPage}
+        />
       </div>
     </div>
   );
